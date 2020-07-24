78d534947d4383b20d9c8cd77b5c7f54edbe90ec
Leonardo Daza <leo.daza.769@gmail.com>
2020-07-24 11:30:26 -0400

    [develop] Added CRUD methods to Class service.

--- src/main/java/com/truextend/problem_1/services/ClassService.java
+++ src/main/java/com/truextend/problem_1/services/ClassService.java
@@ -5,6 +5,7 @@ import com.truextend.problem_1.entities.Class;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ClassService {
 
@@ -16,5 +17,27 @@ public class ClassService {
                     new Class(4,"Principles of computational geo-location analysis","virtual class")
             )
     );
+
+    public Class createClass(Class classes) {
+        classList.add(classes);
+        return classes;
+    }
+
+    public List<Class> readClassAll() {
+        return classList;
+    }
+
+    public Class readClass(int code) {
+        return classList.stream().filter(classStream -> classStream.getCode() == code).collect(Collectors.toList()).get(0);
+    }
+
+    public Class updateClass(Class classes, int code) {
+        classList.stream().filter(classStream -> classStream.getCode() == code).collect(Collectors.toList()).set(0,classes);
+        return classes;
+    }
+
+    public void deleteClass(int code) {
+        classList.removeIf(classStream -> classStream.getCode() == code);
+    }
 
 }
