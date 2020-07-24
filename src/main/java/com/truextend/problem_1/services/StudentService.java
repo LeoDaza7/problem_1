faac9f4b4fcf80c9b0c1c5b9d2aa84c51a3e8f0d
Leonardo Daza <leo.daza.769@gmail.com>
2020-07-24 11:05:52 -0400

    [develop] Added CRUD methods to Student service class.

--- src/main/java/com/truextend/problem_1/services/StudentService.java
+++ src/main/java/com/truextend/problem_1/services/StudentService.java
@@ -5,6 +5,7 @@ import com.truextend.problem_1.entities.Student;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentService {
 
@@ -12,8 +13,30 @@ public class StudentService {
             Arrays.asList(
                     new Student(0,"Wilson","John"),
                     new Student(1,"Graham","Jane"),
-                    new Student(2,"Wilson","John")
+                    new Student(2,"Bam","Pam")
             )
     );
+
+    public Student createStudent(Student student) {
+        studentList.add(student);
+        return student;
+    }
+
+    public List<Student> readStudents(){
+        return studentList;
+    }
+
+    public Student readStudent(int studentId) {
+        return studentList.stream().filter(student -> student.getStudentID() == studentId).collect(Collectors.toList()).get(0);
+    }
+
+    public Student updateStudent(Student student, int studentId) {
+        studentList.stream().filter(studentStream -> studentStream.getStudentID() == studentId).collect(Collectors.toList()).set(0,student);
+        return student;
+    }
+
+    public void deleteUser(int studentId) {
+        studentList.removeIf(student -> student.getStudentID() == studentId);
+    }
 
 }
