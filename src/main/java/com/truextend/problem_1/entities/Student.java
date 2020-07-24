cd914ebe406593f2b7500667f488ef4caffbe1e0
Leonardo Daza <leo.daza.769@gmail.com>
2020-07-24 09:58:21 -0400

    [develop] Added lombok annotations to Student entity.

--- src/main/java/com/truextend/problem_1/entities/Student.java
+++ src/main/java/com/truextend/problem_1/entities/Student.java
@@ -1,5 +1,14 @@
 package com.truextend.problem_1.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     private int studentID;
